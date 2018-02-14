20a1776c87573787adb2e89dc0c33d15ce03330a
KoleTack <kole.tackney@gmail.com>
2018-02-14 02:21:18 -0500

    finish
    -finished scripts

---
+++ New Unity Project/Assets/Scripts/Counting.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counting : MonoBehaviour
+{
+    public int m_CurrentCherries = 2;
+    public Text m_DialogueBox;
+
+    public void IncreaseCherries()
+    {
+        m_CurrentCherries++;
+        m_DialogueBox.text = "How about please with " + m_CurrentCherries.ToString() + " cherries on top?";
+    }
+
+	
+}
---
+++ New Unity Project/Assets/Scripts/StartScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreen : MonoBehaviour
+{
+
+    public void LoadFirstLevel()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+
+    
+
+}
